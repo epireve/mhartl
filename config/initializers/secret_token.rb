da27dcbaca2a17ea781c5fd47bac83686842a800
Firdaus Adib <i@firdaus.my>
2013-02-26 01:48:57 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mhartl::Application.config.secret_token = 'a06b3222e29135bcfc4a7f7083f5929a7ee82c68ffb05cd9a30e15abce7b95d5f7ea15dafb82d7fa956a28a553e5583d656cb88d3a6a6ea39252c348399103b2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mhartl::Application.config.session_store :cookie_store, key: '_mhartl_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mhartl::Application.config.session_store :active_record_store
